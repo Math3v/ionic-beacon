33047abb7b2cca927e15c76d0dc3f881abb90b6d
Matej Minarik <mato.minarik@azet.sk>
2017-10-07 20:11:51 +0200

    Initial version

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {
+  IBeacon,
+  IBeaconDelegate,
+  IBeaconPluginResult,
+  Beacon
+} from '@ionic-native/ibeacon';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +13,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  beaconStatus: string = '';
+  delegate: IBeaconDelegate;
+  beacons: Array<Beacon> = [];
 
+  private unsubscribe: Function;
+
+  constructor(
+    public navCtrl: NavController,
+    private beacon: IBeacon
+  ) {
+    this.beacon.requestAlwaysAuthorization()
+    .then(_ => this.beaconStatus = 'Authorized')
+    .catch(err => this.setBeaconError(err));
   }
 
+  startMonitoring() {
+    this.delegate = this.beacon.getDelegate();
+    const sub = this.delegate.didRangeBeaconsInRegion()
+    .subscribe((result: IBeaconPluginResult) => {
+      this.beacons = result.beacons;
+    });
+
+    this.unsubscribe = sub.unsubscribe;
+  }
+
+  stopMonitoring() {
+    if(this.unsubscribe) {
+       this.unsubscribe();
+    }
+  }
+
+  private setBeaconError(err) {
+    this.beaconStatus = JSON.stringify(err);
+  }
 }
